32b07d60c54d37b16849e7c4acc2c79c026a4508
YeshayahuBennun <ybennun@yahoo.com>
2021-09-23 00:33:24 +0300

    commit#2 - DictionaryApp - User Interface

---
+++ app/src/main/java/com/ybennun/dictionaryapp/WordDefinitionActivity.kt
@@ -0,0 +1,11 @@
+package com.ybennun.dictionaryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WordDefinitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_definition)
+    }
+}
