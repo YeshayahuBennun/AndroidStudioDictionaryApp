30804de5b21cb78eaf9ef162a3574ef547dcdce8
YeshayahuBennun <ybennun@yahoo.com>
2021-09-23 15:43:17 +0300

    commit#3 -  Use Volley Library to Create a Network             Connection

--- app/src/main/java/com/ybennun/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/ybennun/dictionaryapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.ybennun.dictionaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.Method
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val queue = Volley.newRequestQueue(this)
+
+        find_button.setOnClickListener {
+
+            val word = word_edit_tex.text
+            val apiKey = "25c1bbbe-665e-426d-b5c7-9f7050cf6b41"
+            var url =
+                "https://www.dictionaryapi.com/api/v3/references/learners/json/$word?key=$apiKey"
+
+            val stringRequest =
+                StringRequest(Request.Method.GET, url, Response.Listener { response ->
+                    response
+                },
+                    Response.ErrorListener { error ->
+                    error
+                    }
+                )
+            queue.add(stringRequest)
+        }
     }
 }
