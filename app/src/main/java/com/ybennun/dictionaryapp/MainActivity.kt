8cb462eac3d172d371024c98af9d5c3aef50ef91
YeshayahuBennun <ybennun@yahoo.com>
2021-09-25 19:58:21 +0300

    commit#4 -  Extract Word Definition from JSON String

--- app/src/main/java/com/ybennun/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/ybennun/dictionaryapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ybennun.dictionaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
 import java.lang.reflect.Method
 
 class MainActivity : AppCompatActivity() {
@@ -24,14 +27,21 @@ class MainActivity : AppCompatActivity() {
                 "https://www.dictionaryapi.com/api/v3/references/learners/json/$word?key=$apiKey"
 
             val stringRequest =
-                StringRequest(Request.Method.GET, url, Response.Listener { response ->
-                    response
+                StringRequest(Request.Method.GET, url, { response ->
+                    extractDefinitionFromJason(response)
                 },
-                    Response.ErrorListener { error ->
-                    error
+                    { error ->
+                        Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
                     }
                 )
             queue.add(stringRequest)
         }
+    }
+
+    private fun extractDefinitionFromJason(response: String) {
+        val jsonArray = JSONArray(response)
+        val firstIndex = jsonArray.getJSONObject(0)
+        val getShortDefinition = firstIndex.getJSONArray("shortdef")
+        val firstShortDefinition = getShortDefinition.get(0)
     }
 }
